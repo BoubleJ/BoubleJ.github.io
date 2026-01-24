1d89d9206c2b9a44a9c835cd84e782b4aebf9090
BounbleJ <dogmnil2007@naver.com>
2026-01-24 13:11:57 +0900

    fix: gatsby-browser.js와 gatsby-ssr.js에서 React.createElement 사용으로 변경

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,7 @@
 // Gatsby browser API
 import "prism-themes/themes/prism-ghcolors.min.css";
+import Layout from "./src/components/Common/Layout";
 
+export const wrapPageElement = ({ element, props }) => {
+    return <Layout {...props}>{element}</Layout>;
+};
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,6 @@
+import React from "react";
 import Layout from "./src/components/Common/Layout";
 
 export const wrapPageElement = ({ element, props }) => {
-    return <Layout {...props}>{element}</Layout>;
+  return React.createElement(Layout, props, element);
 };
